cbf8b4a412975849dc69743caefd48eb87f85149
Liam Keegan <liam@keegan.ch>
2024-03-21 15:20:57 +0100

    Update formFields.ts with module description field

--- src/lib/formFields.ts
+++ src/lib/formFields.ts
@@ -2,5 +2,6 @@ import type { FormFields } from '$lib/formTypes';
 
 export const formFields: FormFields = {
 	'Lecture name in transcript': '',
-	'CPs of lecture': 0
+	'CPs of lecture': 0,
+	'Module description': '',
 };
