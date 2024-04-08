a8f48d12cf06c3d17b7b2dbc436845b19e071cd9
fsadlo <43894770+fsadlo@users.noreply.github.com>
2024-04-08 17:41:25 +0200

    Extended CP column title to include equivalents.

--- src/lib/formFields.ts
+++ src/lib/formFields.ts
@@ -2,6 +2,6 @@ import type { FormFields } from '$lib/formTypes';
 
 export const formFields: FormFields = {
 	'Lecture name in transcript': '',
-	'CPs of lecture': 0,
+	'CPs of lecture (or equivalent)': 0,
 	'Module description': '',
 };
