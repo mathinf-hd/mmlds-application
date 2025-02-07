04c9309712c7a8009eec7058d2e5e8a89faf2748
fsadlo <43894770+fsadlo@users.noreply.github.com>
2025-02-07 12:37:02 +0100

    Layout

--- src/lib/components/FormExtentDetails.svelte
+++ src/lib/components/FormExtentDetails.svelte
@@ -11,11 +11,11 @@
 		As the admission regulations require 56 ECTS credit points in computer science and 16 ECTS credit points in mathematics, your credits (as listed in your transcript and entered in this form) will be converted automatically to ECTS. For this conversion, you have to enter the standard period of study in months (not the time that you needed to finish your individual study) of your bachelor and the standard number of credits points that have to be earned for your bachelor.
 	</P>
 	<div class="flex gap-8 items-center">
-		<div class="w-40"><P class="font-bold">Standard period of your study program in months (not the time it took you to finish your studies)</P></div>
+		<div class="w-120"><P class="font-bold">Standard period of your study program in months (not the time it took you to finish your studies)</P></div>
 		<PositiveNumberInput bind:value={$data["extentDetails"]["duration"]} class="w-20 text-2xs text-center" />
 	</div>
 	<div class="flex gap-8 items-center">
-		<div class="w-40"><P class="font-bold">Total number of credit points required in your study program (no grades, no grade point averages etc.)</P></div>
+		<div class="w-120"><P class="font-bold">Total number of credit points required in your study program (no grades, no grade point averages etc.)</P></div>
 		<PositiveNumberInput bind:value={$data["extentDetails"]["points"]} class="w-20 text-2xs text-center" />
 	</div>
 </div>
