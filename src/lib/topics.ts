610df98fca2c4d6fc905190707c02082a55c0fda
Jan Fuhrmann <88653871+fuhrmannJ@users.noreply.github.com>
2025-02-27 12:40:25 +0100

    Update topics.ts
    updated topic list
    added subtopics for functional analysis and statistics/prob theory

--- src/lib/topics.ts
+++ src/lib/topics.ts
@@ -1,101 +1,42 @@
 export const formTopics: Topics = [
 	{
-		name: 'Practical computer science',
+		name: 'Differential Geometry',
 		subtopics: [
-			'Technical and formal basics of programming',
-			'Syntax and semantics of programming languages',
-			'Abstraction and specialization',
-			'Specification and verification of algorithms',
-			'Termination',
-			'Complexity analysis'
+			
 		]
 	},
 	{
-		name: 'Technical computer science',
+		name: 'Functional Analysis',
 		subtopics: [
-			'Boolean algebra',
-			'Digital circuits',
-			'Sequential logic',
-			'Number representation and encoding',
-			'Instruction pipelining',
-			'Branch predictor'
+			'Continuation of uniformly continuous maps',
+			'Banach spaces and linear operators',
+			'Duality and weak topology',
+			'Measure theory and Lp spaces',
+			'Hilbert spaces',
+			'Spectral theory of compact operators'			
 		]
 	},
 	{
-		name: 'Algorithms and data structures',
+		name: 'Numerical Analysis',
 		subtopics: [
-			'Fundamentals of algorithms',
-			'Run time analysis',
-			'Basic data structures',
-			'Sorting and hashing',
-			'Graph algorithms',
-			'Search'
+			
 		]
 	},
 	{
-		name: 'Operating systems and networks',
+		name: 'Optimization',
 		subtopics: [
-			'Processes and scheduling',
-			'Memory management',
-			'Concurrency and deadlocks',
-			'APIs, file systems, and I/O',
-			'Communication and routing',
-			'TCP/IP and other protocols'
+			
 		]
 	},
 	{
-		name:'Software engineering',
+		name:'Statistics and Probability Theory',
 		subtopics: [
-			'Requirements engineering',
-			'Software design and modeling',
-			'Quality management',
-			'Evolution',
-			'Software development process',
-			'Project management'
-		]
-	},
-	{
-		name: 'Theoretical computer science',
-		subtopics: [
-			'Computability',
-			'Turing machines',
-			'Halting problem',
-			'Finite automaton',
-			'Theory of formal languages',
-			'Complexity theory'
-		]
-	},
-	{
-		name: 'Databases',
-		subtopics: [
-			'Architecture and functionality of database systems',
-			'Relational database model',
-			'Query processing and optimization',
-			'SQL',
-			'Entity-relationship model',
-			'Physical data organization'
-		]
-	},
-	{
-		name: 'Linear algebra',
-		subtopics: [
-			'Basics of linear algebra',
-			'Vector spaces',
-			'Linear transformations',
-			'Applications of linear transformations',
-			'Inner product spaces',
-			'Applications of inner product spaces'
-		]
-	},
-	{
-		name: 'Calculus',
-		subtopics: [
-			'Complex numbers',
-			'Sequences and series',
-			'Continuity, limits, and Taylor expansion',
-			'Derivatives and mean value theorem',
-			'Fundamental theorem of calculus',
-			'Integrals and basics of multidimensional analysis'
+			'Radon-Nikodym theorem',
+			'Markov kernels',
+			'Martingales',
+			'Neyman-Pearson theory',
+			'Maximum likelihood estimation',
+			'Asymptotic normality'
 		]
 	}
 ]
