7df0dfae23c8c76864b72c1ee1730ecba21fb3e1
Jan Fuhrmann <88653871+fuhrmannJ@users.noreply.github.com>
2025-02-28 11:56:03 +0100

    Update topics.ts
    Added dummy names for topics from DG, Num, Opt

--- src/lib/topics.ts
+++ src/lib/topics.ts
@@ -2,7 +2,12 @@ export const formTopics: Topics = [
 	{
 		name: 'Differential Geometry',
 		subtopics: [
-			
+			'Topic D1',
+			'Topic D2',
+			'Topic D3',
+			'Topic D4',
+			'Topic D5',
+			'Topic D6'
 		]
 	},
 	{
@@ -19,13 +24,23 @@ export const formTopics: Topics = [
 	{
 		name: 'Numerical Analysis',
 		subtopics: [
-			
+			'Topic N1',
+			'Topic N2',
+			'Topic N3',
+			'Topic N4',
+			'Topic N5',
+			'Topic N6'
 		]
 	},
 	{
 		name: 'Optimization',
 		subtopics: [
-			
+			'Topic O1',
+			'Topic O2',
+			'Topic O3',
+			'Topic O4',
+			'Topic O5',
+			'Topic O6'
 		]
 	},
 	{
