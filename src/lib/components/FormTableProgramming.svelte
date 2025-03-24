98a6d019f9307801cce945a31ce9c53329597e43
Jan JOHANNES <contact@janjohannes.de>
2025-03-24 01:52:36 +0100

    Carte Skills

--- src/lib/components/FormTableProgramming.svelte
+++ src/lib/components/FormTableProgramming.svelte
@@ -43,9 +43,9 @@ you acquired skills in areas such as Algorithms and data structures through corr
 			<TableBody>
 				{#each $data.cslectures as Cslecture, CslectureIdx}
 				<TableBodyRow>
-                    <TableBodyCell class="p-2"><GenericValidatedInput type="text" bind:value={Cslecture.name} validateFn={() => checkDuplicateLecture(CslectureIdx)} class="text-2xs"/></TableBodyCell>
+                    <TableBodyCell class="p-2"><GenericValidatedInput type="text" bind:value={Cslecture.name} validateFn={() => checkDuplicateCslecture(CslectureIdx)} class="text-2xs"/></TableBodyCell>
                     <TableBodyCell class="p-2 text-2xs"><Input type="text" class="text-2xs" bind:value={Cslecture.description} /></TableBodyCell>
-		    <TableBodyCell class="p-2"><Button color="red" size="xs" class="text-2xs" on:click={() => deleteCsLecture(CslectureIdx)}><TrashBinOutline /></Button></TableBodyCell>
+		    <TableBodyCell class="p-2"><Button color="red" size="xs" class="text-2xs" on:click={() => deleteCslecture(CslectureIdx)}><TrashBinOutline /></Button></TableBodyCell>
 				</TableBodyRow>
 				{/each}
 			</TableBody>
@@ -53,49 +53,3 @@ you acquired skills in areas such as Algorithms and data structures through corr
 	<Button class="text-2xs m-2" on:click={() => addCslecture()}>Add Another Lecture</Button>
 </div>
 
-<P> Please check the box <input type="checkbox" bind:checked={$data.osproject.check}>, if you actively contributed to an open source project. To declare these skills, add for each respective open source project the name of the project, the link to a public repository and your identifier.
-</P> 
-
-<div class="my-5">
-    <Table class="overflow-x-auto" striped={true}>	
-			<TableHead class="normal-case bg-primary-700 text-white">
-				<TableHeadCell class="min-w-60 text-2xs p-2">Name of open source project</TableHeadCell>
-				<TableHeadCell class="text-2xs p-2">Link to public repository</TableHeadCell>
-				<TableHeadCell class="text-2xs p-2">Personal identifier</TableHeadCell>
-				<TableHeadCell class="text-2xs p-2"></TableHeadCell>
-			</TableHead>
-			<TableBody>
-				{#each $data.osprojects as Osproject, OsprojectIdx}
-				<TableBodyRow>
-                    <TableBodyCell class="p-2"><GenericValidatedInput type="text" bind:value={Osproject.name} validateFn={() => checkDuplicateOsproject(OsprojectIdx)} class="text-2xs"/></TableBodyCell>
-		    <TableBodyCell class="p-2"><GenericValidatedInput type="text" bind:value={Osproject.link} class="text-2xs"/></TableBodyCell>	
-		    <TableBodyCell class="p-2"><GenericValidatedInput type="text" bind:value={Osproject.id} class="text-2xs"/></TableBodyCell>	
-		    <TableBodyCell class="p-2"><Button color="red" size="xs" class="text-2xs" on:click={() => deleteOsproject(OsprojectIdx)}><TrashBinOutline /></Button></TableBodyCell>
-				</TableBodyRow>
-				{/each}
-			</TableBody>
-	</Table>
-	<Button class="text-2xs m-2" on:click={() => addOsproject()}>Add Another Project</Button>
-</div>
-
-<P> Please check the box <input type="checkbox" bind:checked={$data.plecture.check}>, if you particpate in an open source project. To declare these skills, add for each respective lecture its English name as listed in the (translated) transcript. Copy and paste the entire official description of the lecture (as, e.g., provided in the module handbook of your field of study) to the "Module Description" field (after translation to English using some automatic translation service, in case it is not given in English).</P> 
-
-<div class="my-5">
-    <Table class="overflow-x-auto" striped={true}>	
-			<TableHead class="normal-case bg-primary-700 text-white">
-				<TableHeadCell class="min-w-60 text-2xs p-2">Lecture Name in Transcript</TableHeadCell>
-				<TableHeadCell class="text-2xs p-2">Module Description</TableHeadCell>
-				<TableHeadCell class="text-2xs p-2"></TableHeadCell>
-			</TableHead>
-			<TableBody>
-				{#each $data.plectures as Plecture, PlectureIdx}
-				<TableBodyRow>
-                    <TableBodyCell class="p-2"><GenericValidatedInput type="text" bind:value={Plecture.name} validateFn={() => checkDuplicatePlecture(PlectureIdx)} class="text-2xs"/></TableBodyCell>
-                    <TableBodyCell class="p-2 text-2xs"><Input type="text" class="text-2xs" bind:value={Plecture.description} /></TableBodyCell>
-		    <TableBodyCell class="p-2"><Button color="red" size="xs" class="text-2xs" on:click={() => deletePlecture(PlectureIdx)}><TrashBinOutline /></Button></TableBodyCell>
-				</TableBodyRow>
-				{/each}
-			</TableBody>
-	</Table>
-	<Button class="text-2xs m-2" on:click={() => addPlecture()}>Add Another Lecture</Button>
-</div>
