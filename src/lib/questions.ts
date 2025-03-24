0081b844d7f9136d343472f318b1f4fcda77d6e6
Jan JOHANNES <contact@janjohannes.de>
2025-03-24 11:25:51 +0100

    Carte Skills

--- src/lib/questions.ts
+++ src/lib/questions.ts
@@ -1,5 +1,7 @@
 export const formQuestions: Questions = [
 	'Why would you prefer Heidelberg over other highly ranked Universities in Germany? (Max. two sentences)',
-	'What topics in the MSc Mathematics of Machine Learning and Data Science in Heidelberg attract you most? Explain Why. (Max. four sentences)',
-	'Describe in three to four sentences the topic and main outcome of your BSc thesis. (Max. four sentences)'
+	'What topics in the MSc in Heidelberg attract you most? Explain Why. (Max. four sentences)',
+	'Describe in three to four sentences the topic and main outcome of your BSc thesis.',
+	'Discuss how your previous academic and extracurricular experiences have prepared you for the rigorous curriculum of the MSc in Data and Computer Science at Heidelberg University. (Max. four sentences)',
+	'Describe a past experience where you successfully worked as part of a team on a technical project. What role did you play, and what was the outcome? (Max. four sentences)'
 ];
