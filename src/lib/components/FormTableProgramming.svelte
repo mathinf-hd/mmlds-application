dc0109f872a68f688d5044ad580e99964b73caa4
Jan JOHANNES <contact@janjohannes.de>
2025-03-24 02:40:23 +0100

    Carte Skills

--- src/lib/components/FormTableProgramming.svelte
+++ src/lib/components/FormTableProgramming.svelte
@@ -32,7 +32,7 @@ let checkedFlag = false;
 
 
 <P>
-  The admission regulations recognize <b> Programming skills</b>. Please <Checkbox inline class="me-2" bind:checked={checkedFlag}> check the box </Checkbox>   if
+  The admission regulations recognize <b> Programming skills</b>. Please check the box <Checkbox inline class="me-2" bind:checked={checkedFlag} />   if
 you acquired skills in areas such as Algorithms and data structures through corresponding foundational lectures. To declare these skills, add for each respective lecture its English name as listed in the (translated) transcript. Copy and paste the entire official description of the lecture (as, e.g., provided in the module handbook of your field of study) to the "Module Description" field (after translation to English using some automatic translation service, in case it is not given in English).
 </P>
 
@@ -56,7 +56,7 @@ you acquired skills in areas such as Algorithms and data structures through corr
 	<Button class="text-2xs m-2" on:click={() => addLecture()}>Add Another Lecture</Button>
 </div>
 
-<P> Please check the box <input type="checkbox" bind:checked={checkedFlag}> if you actively contributed to an open source project. To declare these skills, add for each respective open source project the name of the project, the link to a public repository and your identifier.
+<P> Please check the box <Checkbox inline class="me-2" bind:checked={checkedFlag} /> if you actively contributed to an open source project. To declare these skills, add for each respective open source project the name of the project, the link to a public repository and your identifier.
 </P> 
 
 <div class="my-5">
@@ -81,7 +81,7 @@ you acquired skills in areas such as Algorithms and data structures through corr
 	<Button class="text-2xs m-2" on:click={() => addLecture()}>Add Another Project</Button>
 </div>
 
-<P> Please check the box <input type="checkbox" bind:checked={checkedFlag}> if you particpate in an open source project. To declare these skills, add for each respective lecture its English name as listed in the (translated) transcript. Copy and paste the entire official description of the lecture (as, e.g., provided in the module handbook of your field of study) to the "Module Description" field (after translation to English using some automatic translation service, in case it is not given in English).</P> 
+<P> Please check the box <Checkbox inline class="me-2" bind:checked={checkedFlag} /> if you particpate in an open source project. To declare these skills, add for each respective lecture its English name as listed in the (translated) transcript. Copy and paste the entire official description of the lecture (as, e.g., provided in the module handbook of your field of study) to the "Module Description" field (after translation to English using some automatic translation service, in case it is not given in English).</P> 
 
 <div class="my-5">
     <Table class="overflow-x-auto" striped={true}>	
