e61d4ffa42ac840d4da97b2ec426ed502c41f8c0
Jan JOHANNES <contact@janjohannes.de>
2025-03-25 17:32:27 +0100

    Korrekturen von Christoph und Anna S.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,6 @@
 <div class="flex flex-col md:container md:mx-auto m-2 items-center">
 	<Heading class="my-2 p-4 text-center">Skills, Lectures, and Motivation</Heading>
 
-<P> Please declare the details on your bachelor's field of study, your lectures and skills to be recognized, and provide your answers to the motivation questions, by filling in this form and then clicking "Save form data" at the bottom to save its contents to a file. Subsequently, upload this file to your application in heiCO. <b>Please notice</b> that the form is filled locally on your computer, i.e., your contents are not sent to any server.</P>
 
 	<Card size="xl" class="my-2">
 		<FormIntroDetails />	
